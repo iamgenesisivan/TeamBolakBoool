af7b67fa9aaa010a02d40a1d28310602ed65e1af
babyange <angelyn.paed@gmail.com>
2018-03-21 13:42:53 +0800

    teacher security

--- SchulerPrototype/App_Start/RouteConfig.cs
+++ SchulerPrototype/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SchulerPrototype
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "superadmin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SchulerPrototype/Controllers/superadminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchulerPrototype.Controllers
+{
+    public class superadminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Security()
+        {
+            return View("~/Views/Security/in_out.cshtml");
+        }
+
+        public ActionResult Teacher()
+        {
+
+            return View("~/Views/Teacher/class_attendance.cshtml");
+        }
+    }
+}
