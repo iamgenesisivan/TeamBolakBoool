204501137a31346a8b942d63992822f0b593f3a5
antoniebrave <antoniobravo026@gmail.com>
2018-03-21 18:48:05 +0800

    inOutLog

---
+++ SchulerPrototype/Controllers/SecurityController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SchulerPrototype.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET
+        public ActionResult InOutLog()
+        {
+            return View();
+        }
+    }
+}
